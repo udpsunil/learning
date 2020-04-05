7e79b049a0ce773a6eb222b90faca4b1599651f5
Sunil U <udpsunil@gmail.com>
2020-04-05 23:08:06 +0530

    add functionality to user model

--- go/go-application/models/user.go
+++ go/go-application/models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 //User struct to hold user information
 type User struct {
 	ID        int
@@ -19,8 +24,43 @@ func GetUsers() []*User {
 
 //AddUser returns user added with ID and error value
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New user must not include id or it must be selected")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+//GetUserByID gets the user by id
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+//UpdateUser updates the user
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+// RemoveUserByID remove user by id
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
