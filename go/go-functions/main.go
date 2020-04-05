67cc146d5542d3468e9ae5aa9bf48c4328ec009f
Sunil U <udpsunil@gmail.com>
2020-04-05 21:55:59 +0530

    with return values

--- go/go-functions/main.go
+++ go/go-functions/main.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, Go playground")
 	port := 3000
-	startWebServer(port, 2)
+	status, err := startWebServer(port, 2)
+	fmt.Println("Server is started", status, err)
 }
 
-func startWebServer(port int, numOfRetries int) {
+func startWebServer(port int, numOfRetries int) (int, error) {
 	fmt.Println("Starting webserver...")
 	//do important things
 	fmt.Println("Server started on port", port)
 	fmt.Println("Number of retries", numOfRetries)
+	//return nil
+	return 255, errors.New("Something went wrong")
+
 }
