d2d297709595d61a7ab63f60311173583cff362a
Sunil U <udpsunil@gmail.com>
2020-04-05 22:11:23 +0530

    object orientation, class members, constructors

--- go/go-application/models/user.go
+++ go/go-application/models/user.go
@@ -2,12 +2,25 @@ package models
 
 //User struct to hold user information
 type User struct {
-	ID int
+	ID        int
 	FirstName string
-	LastName string
+	LastName  string
 }
 
 var (
-	users []*User
-	nextID = 1	
+	users  []*User
+	nextID = 1
 )
+
+//GetUsers return users
+func GetUsers() []*User {
+	return users
+}
+
+//AddUser returns user added with ID and error value
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
