92971dbaa1d970edca0e4b9927eb290a6f8c1ac5
Sunil U <udpsunil@gmail.com>
2020-04-11 22:51:49 +0530

    sentdex new playlist on neural network from scratch
    Signed-off-by: Sunil U <udpsunil@gmail.com>

---
+++ python/nn_sentdex/p1.py
@@ -0,0 +1,8 @@
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+# output = input * weights + bias --> basic neural network with 3 inputs & 1 output
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+print(output)
