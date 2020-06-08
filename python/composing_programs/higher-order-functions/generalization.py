746d60039dbe4705901c58de680229fef85e7a4d
Sunil U <udpsunil@gmail.com>
2020-06-08 21:30:28 +0530

    Generalization of computational elements

--- python/composing_programs/higher-order-functions/generalization.py
+++ python/composing_programs/higher-order-functions/generalization.py
@@ -18,3 +18,48 @@ def area_circle(r):
 
 def area_hexagon(r):
     return area(r, 3 * sqrt(3) / 2)
+
+
+def identity(k):
+    return k
+
+
+def cube(k):
+    return pow(k, 3)
+
+
+def summation(n, term):
+    """Sum the first N terms of a sequence.
+
+    Args:
+        n (int): first N natural numbers
+        term (function): function
+    >>> summation(5, cube)
+    225
+    >>> summation(5, identity)
+    15
+    """
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + term(k), k + 1
+    return total
+
+
+def sum_naturals(n):
+    """ Sum the first N natural numbers 
+    >>> sum_naturals(5)
+    15
+    """
+    return summation(n, identity)
+
+
+def sum_cubes(n):
+    """Sum the first N cubes of natural numbers
+
+    Args:
+        n (int): [first N natural numbers]
+
+    >>> sum_cubes(5)
+    225
+    """
+    return summation(n, cube)
